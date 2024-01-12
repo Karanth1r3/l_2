12f97b1387e01664b055e9e32eb3c35003ff00ff
fuck <asdgfsg235@gmail.com>
2024-01-12 11:42:15 +0300

    strategy

---
+++ pattern/04_command.go
@@ -0,0 +1,160 @@
+package pattern
+
+import "fmt"
+
+// sender, command should be layer between sender and logic elements
+type Button struct {
+	command Command
+}
+
+func (b *Button) press() {
+	b.command.execute()
+}
+
+// Simplified app struct
+type Applictation struct {
+	clipboard    string
+	editors      []Editor
+	activeEditor Editor
+	history      CommandHistory
+}
+
+func NewApplication(e *Editor) *Applictation {
+	return &Applictation{
+		clipboard:    "",
+		activeEditor: *e,
+		editors:      []Editor{*e},
+		history:      CommandHistory{},
+	}
+}
+
+// App executes command sent from ui elements
+func (app *Applictation) executeCommand(c Command) {
+	c.execute()
+	app.history.push(c)
+}
+
+// App can undo commands with command history stack
+func (app *Applictation) undo() {
+	command, ok := app.history.pop()
+	if ok {
+		command.undo()
+	}
+}
+
+// Simplified text editor example struct
+type Editor struct {
+	selection string
+}
+
+func newEditor() *Editor {
+	return &Editor{
+		selection: "",
+	}
+}
+
+func (e *Editor) getSelection() string {
+	fmt.Println("Selected text is returned")
+	return e.selection
+}
+
+func (e *Editor) deleteSelection() {
+	fmt.Println("Selected text is deleted")
+	e.selection = ""
+}
+
+func (e *Editor) replaceSelection(s string) {
+	fmt.Println("Insert text from clipboard at selected position")
+	e.selection = s
+}
+
+type CommandsList struct {
+	Applictation
+	Editor
+	CopyCommand
+	CutCommand
+	PasteCommand
+}
+
+type CommandHistory struct {
+	commands []Command
+}
+
+func (ch *CommandHistory) push() {
+
+}
+
+// interface has at least 1 func - execute
+type Command interface {
+	execute()
+	undo()
+	saveBackup()
+}
+
+type TemplateCommand struct { // this shall be embedded in concrete commands to reduce copies
+	app    Applictation
+	editor Editor
+	backup string
+}
+
+func (tc *TemplateCommand) saveBackup() {
+	tc.backup = tc.editor.selection
+}
+
+// concrete command 1
+type CopyCommand struct {
+	TemplateCommand
+}
+
+func NewCopyCommand(app *Applictation, e *Editor) *CopyCommand {
+	return &CopyCommand{
+		TemplateCommand: TemplateCommand{
+			app:    *app,
+			editor: *e,
+			backup: "",
+		},
+	}
+}
+
+func (cp *CopyCommand) execute() {
+	cp.app.clipboard = cp.editor.getSelection()
+}
+
+type CutCommand struct {
+	TemplateCommand
+}
+
+func (ct *CutCommand) execute() {
+	ct.saveBackup()
+	ct.app.clipboard = ct.editor.getSelection()
+	ct.editor.deleteSelection()
+}
+
+type PasteCommand struct {
+	TemplateCommand
+}
+
+func (p *PasteCommand) execute() {
+	p.saveBackup()
+	p.editor.replaceSelection(p.app.clipboard)
+}
+
+type UndoCommand struct {
+	TemplateCommand
+}
+
+func (uc *UndoCommand) undo() {
+	uc.app.undo()
+}
+
+func testCommand() {
+	// initialize editor & application
+	editor := newEditor()
+	app = NewApplication(editor)
+
+	copy := NewCommand(app, editor)
+	paste := NewCommand()
+	// initialize buttons & link initial commands (they can be replaced if needed)
+	copyButton := Button{command: copy}
+
+}
---
+++ pattern/07_strategy.go
@@ -0,0 +1,93 @@
+package pattern
+
+import "fmt"
+
+// Strategy interface
+type Robbery interface {
+	Rob(*Bank)
+}
+
+type CriminalWallet struct {
+	money int
+}
+
+// Concrete strategy 1
+type StealthRobbery struct {
+	CriminalWallet
+	crewStealthMastery int
+	effectiveness      int
+}
+
+// Functionality which can be switched easily with another concrete strategy implementation in service
+func (s *StealthRobbery) Rob(b *Bank) {
+	if s.crewStealthMastery > b.awareness {
+		b.money -= s.effectiveness
+		s.money += s.effectiveness
+	} else {
+		s.crewStealthMastery -= 10
+		s.effectiveness -= 10
+		b.defenseSystemIndex += 5
+		b.awareness += 20
+		fmt.Println("Stealth robbery failed. Bank security systems have been improved")
+	}
+}
+
+type ArmedRobbery struct {
+	CriminalWallet
+	crewGunfightMastery int
+	effectiveness       int
+}
+
+func (a *ArmedRobbery) Rob(b *Bank) {
+	if a.crewGunfightMastery > b.defenseSystemIndex {
+		b.money -= a.effectiveness
+		a.money += a.effectiveness
+		fmt.Println("Armed robbery was successfull")
+	} else {
+		a.crewGunfightMastery -= 10
+		a.effectiveness -= 10
+		b.defenseSystemIndex += 20
+		b.awareness += 5
+		fmt.Println("Armed robbery failed. Bank security systems have been improved")
+	}
+}
+
+// Service
+type CriminalCrew struct {
+	robStrat Robbery
+}
+
+// Service can change behaviour strategy by assigning different ones
+func (c *CriminalCrew) setRobberyStrategy(r Robbery) {
+	c.robStrat = r
+}
+
+// This one actually is not connected to strategy pattern but in this example it is necessary
+type Bank struct {
+	money, defenseSystemIndex, awareness int
+}
+
+func NewBank(money, defense, awareness int) *Bank {
+	return &Bank{
+		money:              money,
+		defenseSystemIndex: defense,
+		awareness:          awareness,
+	}
+}
+
+func testStrategy() {
+	bank := NewBank(50000, 15, 25)
+
+	wallet := &CriminalWallet{0}
+
+	armedRob := ArmedRobbery{*wallet, 50, 750}
+	stealthRob := StealthRobbery{*wallet, 24, 1000}
+
+	// Trying with the first strategy
+	crew := CriminalCrew{&stealthRob}
+	crew.robStrat.Rob(bank)
+	// Changing strategy
+	crew.setRobberyStrategy(&armedRob)
+	crew.robStrat.Rob(bank) // This one should be successfull
+
+}
