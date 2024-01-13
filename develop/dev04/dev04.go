aad226eecc6c51409acca8967bf6d1a8c0a916ad
fuck <asdgfsg235@gmail.com>
2024-01-13 23:08:11 +0300

    dev04_second_iteration

--- develop/dev04/dev04.go
+++ develop/dev04/dev04.go
@@ -8,17 +8,16 @@ import (
 )
 
 func main() {
-	dict := []string{"Fuck", "Uckf", "Care"}
+	dict := []string{"Fuck", "Uckf", "Care", "cufk", "Тяпка", "пятка", "пятак"}
 	dictToLower(dict)
-	fmt.Println(dict)
 	t := getAnagramGroups(&dict)
 	fmt.Println(t)
 
 }
 
 func dictToLower(dict []string) {
-	for _, elem := range dict {
-		elem = strings.ToLower(elem)
+	for i := 0; i < len(dict); i++ {
+		dict[i] = strings.ToLower(dict[i])
 	}
 }
 
@@ -26,13 +25,22 @@ func hasEqualRuneCount(s1, s2 string) bool {
 	return utf8.RuneCountInString(s1) == utf8.RuneCountInString(s2)
 }
 
-func toLowercase(s string) string {
-	return strings.ToLower(s)
-}
-
 func getAnagramGroups(dct *[]string) (result map[string][]string) {
 	result = make(map[string][]string)
-	for _, elem := range *dct {
+
+	go func() {
+
+	}()
+out:
+	for _, elem := range *dct { // ...
+		for _, slc := range result {
+			for _, val := range slc {
+				if val == elem {
+					continue out
+				}
+			}
+		}
+
 		findAnagramGroup(elem, dct, result)
 	}
 	return result
@@ -40,13 +48,13 @@ func getAnagramGroups(dct *[]string) (result map[string][]string) {
 
 func findAnagramGroup(word string, dct *[]string, anagrams map[string][]string) {
 	result := make(map[string][]string)
+	foundIndexes := []int{}
+
 	for index, elem := range *dct {
 		if elem == word { // if words are equal - they aren't anagrams
-			fmt.Println("==")
 			continue
 		}
 		if !hasEqualRuneCount(elem, word) { // if words have different symbol count - they aren't anagrams
-			fmt.Println("==Count")
 			continue
 		}
 		chars := []rune(elem)
@@ -57,14 +65,33 @@ func findAnagramGroup(word string, dct *[]string, anagrams map[string][]string) 
 		sort.Slice(chars, func(i, j int) bool {
 			return chars[i] < chars[j]
 		})
-		fmt.Println(string(chars), string(orChars))
 		if string(orChars) == string(chars) { // if sorted strings are equal - they are anagrams. Check for original word & original element equality was before
-			fmt.Println("Found")
 			result[word] = append(result[word], elem)
-			*dct = append((*dct)[:index], (*dct)[index+1:]...)
+			foundIndexes = append(foundIndexes, index)
 		}
 	}
-	if len(result) > 1 { // if  group has at least 2 elements, it can be added to anagrams map
-		anagrams[word] = result[word]
+	if len(result[word]) > 1 { // if  group has at least 2 elements, it can be added to anagrams map
+		rs := []string{}
+		rs = append(rs, result[word]...)
+		anagrams[word] = rs
 	}
+
+	/*// block for deleting found elements from dictionary
+	  for _, e := range foundIndexes {
+	    *dct = slices.Delete(*dct, e)
+	  }
+	*/
+	/*
+	   for _, e := range result[word] {
+	     for i, e2 := range *dct {
+	       if e == e2 {
+	         //fmt.Println(*dct, i)
+	         *dct = append((*dct)[:i], (*dct)[i+1:]...)
+	         //fmt.Println("deleted")
+	       }
+	     }
+	   }
+	*/
+	fmt.Println(*dct)
+
 }
